13d53a7a99292dd57421796fa6f4f02c89c9f713
ltbur <ltburtonst@gmail.com>
2022-08-05 14:07:44 +0900

    ファイル作成

---
+++ Assets/Pusher.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pusher : MonoBehaviour
+{
+    Vector3 startPosition;
+    public float amplitude;
+    public float speed;
+    
+    void Start()
+    {
+        startPosition = transform.localPosition;    
+    }
+    // Update is called once per frame
+    void Update()
+    {
+       float z = amplitude * Mathf.Sin(Time.time * speed);
+
+       transform.localPosition = startPosition + new Vector3(0,0,z); 
+    }
+}
